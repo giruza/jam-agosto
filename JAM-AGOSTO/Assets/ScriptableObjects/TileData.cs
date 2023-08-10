f3a78dc0afb2a5b0ad9c5864a793d0aeaacbcd37
JoseAntonioMM <joseantoniomm1997.ja@gmail.com>
2023-08-11 01:14:57 +0200

    Creacion de objeto scripteable

---
+++ JAM-AGOSTO/Assets/ScriptableObjects/TileData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TileMaps;
+
+public class NewBehaviourScript : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
