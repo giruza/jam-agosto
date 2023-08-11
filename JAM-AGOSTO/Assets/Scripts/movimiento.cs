e9abfd3dd34432d9c2ef57e4175f4d316de5ea05
JoseAntonioMM <joseantoniomm1997.ja@gmail.com>
2023-08-11 18:37:58 +0200

    Escena inicial - elementos basicos
    Añadido y configurado URP
    Añadido sprites y ordenacion en base a la posicion del pivot
    Configurado tipo de renderizado por sprite para pixelart
    Añadido script de movimiento basico

---
+++ JAM-AGOSTO/Assets/Scripts/movimiento.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left; ;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.down;
+        }
+    }
+}
