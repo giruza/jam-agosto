5ce0d4b9616f38ce90e497a59668dee9ed393752
JoseAntonioMM <joseantoniomm1997.ja@gmail.com>
2023-08-11 12:33:43 +0200

    Update TileData.cs

--- JAM-AGOSTO/Assets/ScriptableObjects/TileData.cs
+++ JAM-AGOSTO/Assets/ScriptableObjects/TileData.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TileMaps;
+using UnityEngine.Tilemaps;
 
-public class NewBehaviourScript : ScriptableObject
+public class TileData : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
