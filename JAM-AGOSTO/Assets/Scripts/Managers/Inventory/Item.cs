e6972aa8a2064f0ff16cb985ef95e261809a9299
AlvaroMK <alvaro.ajurado@alumnos.upm.es>
2023-08-29 18:45:56 +0200

    Panel de mejoras de la tienda comenzado
    Falta por crear un panel de información detallada cuando pulses cada objeto, así como su sistema de compra o "re-rolleo", pero la base esta creada.

--- JAM-AGOSTO/Assets/Scripts/Managers/Inventory/Item.cs
+++ JAM-AGOSTO/Assets/Scripts/Managers/Inventory/Item.cs
@@ -9,4 +9,7 @@ public class Item : ScriptableObject
     public string itemDescription;  // descripción del objeto
     public int value;               // valor del objeto (que usara en caso de mejorar algun stat o recuperar vida)
     public Sprite icon;             // icono del objeto
+    public int phase;               // fase actual a la que tenemos mejorada el objeto
+    public int maxPhase;            // fase maxima hasta la que se puede mejorar el objeto
+    public int price;               // precio por el cual podemos mejorar una fase del objeto (mirar si queremos que siempre sea el mismo)
 }
